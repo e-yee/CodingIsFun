943af2754abe2ba4aed3cb9afbdbd7bdccad073e
e-yee <viduydmm@gmail.com>
2025-06-06 11:16:47 +0700

    feat: add Trending section

--- ReactTutorial/my-first-react-app/src/App.jsx
+++ ReactTutorial/my-first-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import Spinner from './components/Spinner'
 import MovieCard from './components/MovieCard'
 import { useDebounce } from 'react-use';
+import { getTrendingMovies, updateSearchCount } from './appwrite'
 
 const API_BASE_URL ='https://api.themoviedb.org/3';
 
@@ -17,11 +18,16 @@ const API_OPTIONS = {
   }
 }
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
   const [debouncedSearchTerm, setDebouncedSearcTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [trendingErrorMessage, setTrendingErrorMessage] = useState('');
+  const [isTrendingLoading, setIsTrendingLoading] = useState(false);
 
   // Debounce the search term to prevent making too many API requests
   // by waiting for the user to stop typing for 500ms
@@ -51,6 +57,10 @@ const App = () => {
       }
 
       setMovies(data.results || []);
+      
+      if(query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.')
@@ -59,15 +69,39 @@ const App = () => {
     }
   }
 
+  const loadTrendingMovies = async () => {
+    setIsTrendingLoading(true);
+    setTrendingErrorMessage('');
+
+    try {
+      const movies = await getTrendingMovies();
+
+      if(movies.length > 0) {
+        setTrendingMovies(movies);
+      } else if(movies.length == 0) {
+        setTrendingErrorMessage('No trending movies');
+        setTrendingMovies([]);
+        return;
+      }
+    } catch (error) {
+      console.error(`Error fetching trending movies: ${error}`);
+      setTrendingErrorMessage('Error fetching trending movies.');
+    } finally {
+      setIsTrendingLoading(false);
+    }
+  }
+
   useEffect(() => {
     fetchMovies(debouncedSearchTerm);
   }, [debouncedSearchTerm]);
 
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
+
   return (
     <main>
-      <div className="pattern">
-
-      </div>
+      <div className="pattern" />
 
       <div className="wrapper">
         <header>
@@ -76,9 +110,28 @@ const App = () => {
           
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
+        
+        <section className="trending">
+          <h2>Trending Movies</h2>
+
+          {isTrendingLoading ? (
+            <Spinner />
+          ) : trendingErrorMessage ? (
+            <p className="text-red-500">{trendingErrorMessage}</p>
+          ) : (
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
 
         <section className="all-movies">
-          <h2 className="mt-[40px]">All Movies</h2>
+          <h2>All Movies</h2>
 
           {isLoading ? (
             <Spinner />
